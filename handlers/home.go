3bf5fd95bc0f82d87761b2518eaf3a15211364b4
xxyztwz <xxyztwz@gmail.com>
2016-02-24 18:16:21 +0800

    把定制化的东西改回来，适合提交社区

--- handlers/home.go
+++ handlers/home.go
@@ -2,13 +2,12 @@ package handlers
 
 import (
 	"net/http"
-
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 	http.Redirect(w, r,
-		"https://github.com/xxyztwz/discovery.etcd.io",
+		"https://coreos.com/docs/cluster-management/setup/cluster-discovery/",
 		http.StatusMovedPermanently,
 	)
 }
